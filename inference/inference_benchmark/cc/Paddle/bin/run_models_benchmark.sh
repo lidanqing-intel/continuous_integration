bc93fd7ef2c678c22aa5eecb672aa592535f701a
lidanqing-intel <danqing.li@intel.com>
2021-05-24 11:14:40 +0200

    remove gpu related command

--- inference/inference_benchmark/cc/Paddle/bin/run_models_benchmark.sh
+++ inference/inference_benchmark/cc/Paddle/bin/run_models_benchmark.sh
@@ -8,7 +8,7 @@ export OUTPUT_BIN=$ROOT/build
 export DATA_ROOT=$ROOT/Data
 export CASE_ROOT=$ROOT/bin
 export LOG_ROOT=$ROOT/log
-export gpu_type=`nvidia-smi -q | grep "Product Name" | head -n 1 | awk '{print $NF}'`
+#export gpu_type=`nvidia-smi -q | grep "Product Name" | head -n 1 | awk '{print $NF}'`
 source $ROOT/bin/run_clas_mkl_benchmark.sh
 # test model type
 model_type="static"
