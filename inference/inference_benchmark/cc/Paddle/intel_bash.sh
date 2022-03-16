07d330065daea2996b83823954d50c2925f1b713
lidanqing-intel <danqing.li@intel.com>
2022-03-16 04:26:05 +0000

    add python required lib in intel_bash.sh

--- inference/inference_benchmark/cc/Paddle/intel_bash.sh
+++ inference/inference_benchmark/cc/Paddle/intel_bash.sh
@@ -1,7 +1,13 @@
 set -xe
+
+python3.7 -m pip install py3nvml;  # monitor gpu
+python3.7 -m pip install cup;      # monitor cpu
+python3.7 -m pip install pandas;   # process data
+python3.7 -m pip install openpyxl; # process data to excel
+
 rm -rf build
 #export LIB_DIR=/mnt/disk500/continuous_integration/inference/inference_benchmark/cc/Paddle/paddle_inference
-export LIB_DIR=/home/li/repo/Paddle/build_12346cd/paddle_inference_install_dir
+export LIB_DIR=/home/li/repo/Paddle/build_d6e99fe/paddle_inference_install_dir
 # export LIB_DIR=/home/li/repo/Paddle/build/paddle_inference_install_dir
 bash compile.sh all ON OFF OFF ${LIB_DIR}
 bash bin/run_models_benchmark.sh "static" "cpu" "1" "1"
